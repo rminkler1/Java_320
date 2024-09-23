78968fcf840ba226886a224be7f5fce8ff97cbf0
robertminkler <robertminkler@gmail.com>
2024-09-22 20:03:37 -0700

    Assignment 8 Update

--- src/Assignments/MinklerModule8.java
+++ src/Assignments/MinklerModule8.java
@@ -3,109 +3,116 @@ Robert Minkler
 September 22, 2024
 CSD 320 Module 8 Assignment
 
-Write four methods that use method overloading
-
-Write a program with four methods for calculating the cost of a yearly auto service visit. The methods will be titled yearlyService.
-
-        yearlyService(no parameters) - Will return the standard service charge.
-        yearlyService(one parameter) - Will return the standard service charge with an added oil change fee.
-        yearlyService(two parameters) - Will return the standard service charge with an added oil change fee and a tire rotation charge.
-        yearlyService(three parameters) - Will return the standard service charge with an added oil change fee, a tire rotation charge, along with a coupon amount that will be deducted from the total cost.
-
+Write four methods that use method overloading.
 Write a main method that will test each of these methods two times.
 */
 
-
-import java.text.DecimalFormat;
 
 public class MinklerModule8 {
 
     // Define constants
     static final double stdServiceChg = 30.00;
-    static final DecimalFormat df = new DecimalFormat("0.00");
-    static final String line = "___________________________________";
+    static final String line = "____________________________________";
 
     public static void main(String[] args) {
 
-        double result;
-
         // Just Standard Annual Service - no parameters
         System.out.println("\n\n--Standard Service with no additions--\n");
-        result = yearlyService();
-        System.out.println("Total:\t\t\t\t\t\t$" + df.format(result));
+        printTotal(yearlyService());
 
-        // Just Standard Annual Service - no parameters 2nd time just for assignment compliance.
-        System.out.println("\n\n--Standard Service with no additions again--\n");
-        result = yearlyService();
-        System.out.println("Total:\t\t\t\t\t\t$" + df.format(result));
 
-        // Standard service and $44.99 oil change
+        // Standard service and oil change X2
         System.out.println("\n\n--Standard Service and oil change #1--\n");
-        result = yearlyService(44.99);
-        System.out.println("Total:\t\t\t\t\t\t$" + df.format(result));
+        printTotal(yearlyService(44.99));
 
-        // Standard service and $65.50 oil change
         System.out.println("\n\n--Standard Service and oil change #2--\n");
-        result = yearlyService(65.50);
-        System.out.println("Total:\t\t\t\t\t\t$" + df.format(result));
+        printTotal(yearlyService(65.50));
 
-        // Standard service and $65.50 oil change & $10 Tire Rotation
+
+        // Standard service, oil change & Tire Rotation X2
         System.out.println("\n\n--Standard Service, oil change & tire rotation #1--\n");
-        result = yearlyService(65.50, 10);
-        System.out.println("Total:\t\t\t\t\t\t$" + df.format(result));
+        printTotal(yearlyService(65.50, 10));
 
-        // Standard service and $45.99 oil change & $14.99 Tire Rotation
         System.out.println("\n\n--Standard Service, oil change & tire rotation #2--\n");
-        result = yearlyService(45.99, 14.99);
-        System.out.println("Total:\t\t\t\t\t\t$" + df.format(result));
+        printTotal(yearlyService(45.99, 14.99));
 
-        // Standard service & $45.99 oil change & $12.99 Tire Rotation & $10 off
+
+        // Standard service, oil change, Tire Rotation, & discount X2
         System.out.println("\n\n--Service, oil change & tire rotation w/ $10 coupon #1--\n");
-        result = yearlyService(45.99, 12.99, 10);
-        System.out.println("Total:\t\t\t\t\t\t$" + df.format(result));
+        printTotal(yearlyService(45.99, 12.99, 10));
 
-        // Standard service & $55.99 oil change & $10.99 Tire Rotation & $5 off
         System.out.println("\n\n--Service, oil change & tire rotation w/ $5 coupon #2--\n");
-        result = yearlyService(55.99, 10.99, 5.00);
-        System.out.println("Total:\t\t\t\t\t\t$" + df.format(result));
-
+        printTotal(yearlyService(55.99, 10.99, 5.00));
     }
 
     public static double yearlyService() {
         // No Parameters - Will return the standard service charge.
         // Print pricing breakdown before returning total
-        System.out.println("Standard Service Charge\t\t$" + df.format(stdServiceChg));
+        printServChg();
+
         System.out.println(line);
+
         return stdServiceChg;
     }
 
     public static double yearlyService(double oilChangeChg) {
         // One Parameter - Will return the standard service charge with an added oil change fee.
         // Print pricing breakdown before returning total
-        System.out.println("Standard Service Charge\t\t$" + df.format(stdServiceChg));
-        System.out.println("Oil Change\t\t\t\t\t$" + df.format(oilChangeChg));
+        printServChg();
+        printOilChg(oilChangeChg);
+
         System.out.println(line);
+
         return stdServiceChg + oilChangeChg;
     }
 
     public static double yearlyService(double oilChangeChg, double tireRotateChg) {
         //  Two Parameters - returns standard service charge with an added oil change fee and a tire rotation charge.
         // Print pricing breakdown before returning total
-        System.out.println("Standard Service Charge\t\t$" + df.format(stdServiceChg));
-        System.out.println("Oil Change\t\t\t\t\t$" + df.format(oilChangeChg));
-        System.out.println("Tire Rotation\t\t\t\t$" + df.format(tireRotateChg));
+        printServChg();
+        printOilChg(oilChangeChg);
+        printTireRot(tireRotateChg);
+
         System.out.println(line);
+
         return stdServiceChg + oilChangeChg + tireRotateChg;
     }
 
     public static double yearlyService(double oilChangeChg, double tireRotateChg, double dolOffCoupon) {
         // Three Parameters - Returns standard service charge + oil change fee + tire rotation charge - coupon amount
         // Print pricing breakdown before returning total
-        System.out.println("Standard Service Charge\t\t$" + df.format(stdServiceChg));
-        System.out.println("Oil Change\t\t\t\t\t$" + df.format(oilChangeChg));
-        System.out.println("Tire Rotation\t\t\t\t$" + df.format(tireRotateChg));
-        System.out.println("Discount Applied\t\t\t($" + df.format(dolOffCoupon) + ")");
+        printServChg();
+        printOilChg(oilChangeChg);
+        printTireRot(tireRotateChg);
+        printDisc(dolOffCoupon);
+
         System.out.println(line);
+
         return stdServiceChg + oilChangeChg + tireRotateChg - dolOffCoupon;
+    }
+
+    public static void printServChg() {
+        // Formatted output Description and price
+        System.out.printf("%-25s $%,8.2f%n", "Standard Service Charge", stdServiceChg);
+    }
+
+    public static void printOilChg(double oilChangeChg) {
+        // Formatted output Description and price
+        System.out.printf("%-25s $%,8.2f%n", "Oil Change", oilChangeChg);
+    }
+
+    public static void printTireRot(double tireRotateChg) {
+        // Formatted output Description and price
+        System.out.printf("%-25s $%,8.2f%n", "Tire Rotation", tireRotateChg);
+    }
+
+    public static void printDisc(double dolOffDisc) {
+        // Formatted output Description and price
+        System.out.printf("%-25s($%,8.2f)%n", "Discount Applied", dolOffDisc);
+    }
+
+    public static void printTotal(double total) {
+        // Formatted output Description and price
+        System.out.printf("%-25s $%,8.2f%n", "Total", total);
     }
 }
